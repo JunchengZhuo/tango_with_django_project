be0e35fd07abed01ff013af0836820610a76d59f
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-07-29 17:53:26 +0100

    test3

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def index(request):
+   return HttpResponse("Rango says hey there partner!<br/> <a href='/rango/about/'>about</a>")
+def about(request):
+   return HttpResponse("Rango says here is about page<br/> <a href='/rango/'>index</a>")
