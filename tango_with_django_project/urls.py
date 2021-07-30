9d94ba259c8c6860cb93d85844c1ed527846a11e
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-07-30 21:24:43 +0100

    test6.12

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,9 +19,10 @@ from django.conf.urls import include
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
-uurlpatterns = [
-path('', views.index, name='index'),
-path('about/', views.about, name='about'),
-path('category/<slug:category_name_slug>/',
-views.show_category, name='show_category'),
+from django.urls import path
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+        views.show_category, name='show_category'),
 ]#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
