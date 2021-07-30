e12daa387325aaf3373d0275ededc0214f978b00
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-07-30 19:11:57 +0100

    test5.4

--- rango/models.py
+++ rango/models.py
@@ -11,5 +11,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     def __str__(self):
         return self.title
