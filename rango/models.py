932e7b5c882f382b897baa8024e650de836bd42e
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-07-30 20:41:45 +0100

    test6.5

--- rango/models.py
+++ rango/models.py
@@ -12,3 +12,11 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
     def __str__(self):
         return self.name
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
