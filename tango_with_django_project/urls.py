f89f48a565ab76c2b8eeb0fea253847b8168ffa1
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-07-31 19:09:21 +0100

    test9.12

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,8 +23,12 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page,
+            name='add_page'),
     path('category/<slug:category_name_slug>/', views.show_category,
-        name='show_category'),
+            name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
+    path('register/', views.register, name='register'), # New mapping!
+    
 ]
 #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
