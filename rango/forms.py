2e6160990803addd6931dda58d7a1d6801fc1d87
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-07-31 19:25:56 +0100

    test9.16

--- rango/forms.py
+++ rango/forms.py
@@ -32,13 +32,13 @@ class UserForm(forms.ModelForm):
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
-    model = UserProfile
-    fields = ('website', 'picture',)
+        model = UserProfile
+        fields = ('website', 'picture',)
     # What fields do we want to include in our form?
     # This way we don't need every field in the model present.
     # Some fields may allow NULL values; we may not want to include them.
     # Here, we are hiding the foreign key.
     # we can either exclude the category field from the form,
-    exclude = ('category',)
+    #exclude = ('category',)
     # or specify the fields to include (don't include the category field).
     #fields = ('title', 'url', 'views')
