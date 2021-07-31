57f1ea752876da2d940f23c125b31dfb8d6cc1b6
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-07-31 18:23:14 +0100

    test9.2

--- rango/models.py
+++ rango/models.py
@@ -21,3 +21,14 @@ class Page(models.Model):
     likes = models.IntegerField(default=0)
     def __str__(self):
         return self.title
+        class UserProfile(models.Model):
+# This line is required. Links UserProfile to a User model instance.
+user = models.OneToOneField(User, on_delete=models.CASCADE)
+# The additional attributes we wish to include.
+website = models.URLField(blank=True)
+picture = models.ImageField(upload_to='profile_images', blank=True)
+def __str__(self):
+return self.user.username
+Note that we reference the User model using a one-to-one relationship. Since we
+reference the default User model, we need to import it within the models.py file:
+from django.contrib.auth.models import User
