b8939e2d843c3bd069981a6714a7fdbfcaaf5937
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-08-01 18:30:11 +0100

    test 9.18

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,26 +16,16 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-#app_name = 'rango'
+from rango import views
+#app_name='rango'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'about/$', views.about, name='about'),
-    url(r'^add_category/$' views.add_category, name='add_category'),
-
-    url(r'^category/(?P<category_ name_ _slug>[\W\-]+)/$',
-            views . show_ category,
-            name= ' show_ category'),
-
-        url(r'^category/(?P<category_name_slug>[\W\-]+)/add_page/$',
-            views.add_page,
-            name='add_page'),
-
-        url(r'^register/$',
-            views.register,
-            name='register'),
-]
-#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'rango/$', views.index, name='index'),
+    url(r'rango/about/', views.about, name='about'),
+    #url(r'rango/category/<slug:category_name_slug>/add_page/', views.add_page,name='add_page'),
+    url(r'rango/category/<slug:category_name_slug>/', views.show_category,name='show_category'),
+    url(r'rango/add_category/', views.add_category, name='add_category'),
+    url(r'rango/register/', views.register, name='register'), # New mapping!
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
