0fe9a3af3976822a0f7b4c4990344e5a4783a6d9
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-08-02 01:16:38 +0100

    test10.9

--- rango/views.py
+++ rango/views.py
@@ -9,9 +9,21 @@ from rango.forms import UserForm, UserProfileForm
 from datetime import datetime
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    return render(request, 'rango/index.html', context=context_dict)
+    context_dict['categories] = category_list
+    context_dict['pages'] = page_list
+
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/index.html', context=context_dict)
+    
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request, response)
+    
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 def about(request):
     if request.session.test_cookie_worked():
     print("TEST COOKIE WORKED!")
