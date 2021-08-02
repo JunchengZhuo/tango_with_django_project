aae3d589eabf214608b04430acdeb0bc9d147ad6
JunchengZhuo <2511736z@student.gla.ac.uk>
2021-08-02 01:28:40 +0100

    test10.11

--- rango/views.py
+++ rango/views.py
@@ -185,10 +185,22 @@ def index(request):
     return val
 
 # Updated the function definition
-    def visitor_cookie_handler(request):
+def visitor_cookie_handler(request):
         visits = int(get_server_side_cookie(request, 'visits', '1'))
         last_visit_cookie = get_server_side_cookie(request,
                                                    'last_visit',
                                                     str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+        last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                         '%Y-%m-%d %H:%M:%S')
+
+        # If it's been more than a day since the last visit...
+        if (datetime.now() - last_visit_time).days > 0:
+            visits = visits + 1
+            # Update the last visit cookie now that we have updated the count
+            request.session['last_visit'] = str(datetime.now())
+        else:
+            # Set the last visit cookie
+            request.session['last_visit'] = last_visit_cookie
+
+        # Update/set the visits cookie
+        request.session['visits'] = visits
